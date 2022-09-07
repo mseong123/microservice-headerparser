7d1b7a31561d18a03dbe94a0759fd472c8c88176
Mseong <mseong123@hotmail.com>
2022-09-07 12:36:26 +0800

    amended method to get valid ipaddress

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const serverless=require('serverless-http');
+var requestIP = require("request-ip")
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
@@ -7,11 +8,12 @@ var app = express();
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
+app.enable("trust proxy");
+
 //API endpoint
 app.get('/api/whoami', function (req, res) {
-  console.log(req.headers)
   res.json({ 
-    ipaddress: req.headers["client-ip"],
+    ipaddress: requestIP.getClientIp(req),
     language: req.headers["accept-language"],
     software:req.headers["user-agent"]
    });
